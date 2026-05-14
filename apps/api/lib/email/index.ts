3dd2197a6592db52bf270f39833ece418b3eb907
vivek chhabra <vivekcb8642@gmail.com>
2026-05-15 00:33:17 +0530

    feat: add necessary email services

---
+++ apps/api/lib/email/index.ts
@@ -0,0 +1,10 @@
+export {
+  EmailServiceError,
+  sendTemplateEmail,
+  sendTextEmail
+} from "./service";
+export { BaseEmail } from "./templates/base-email";
+export {
+  PlainMessageEmail,
+  type PlainMessageEmailProps
+} from "./templates/plain-message-email";
